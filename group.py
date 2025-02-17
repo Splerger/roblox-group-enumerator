c03c625099d48951134ca51d22748d6d23f57ef8
jane <83114316+Splerger@users.noreply.github.com>
2025-02-17 13:53:59 +0000

    mingle

--- group.py
+++ group.py
@@ -1,15 +1,17 @@
 import httpx
 import json
 from time import sleep
-from tabulate import tabulate
 from threading import Thread, Lock
 import os
 import shutil
+from config import roblox_cookie
 
 folder_name = "group"
 output_file = "all_members.json"
-from config import roblox_cookie
-from config import group_ids
+friends_dir = "friends"
+
+with open('group_ids.txt') as f:
+    group_ids = [line.strip() for line in f]
 
 headers = {
     'Cookie': f'.ROBLOSECURITY={roblox_cookie}',
@@ -32,7 +34,7 @@ def send_request(url, params=None, retries=5, backoff_factor=2):
             print(f"Attempt {attempt}: HTTP error {e.response.status_code} on {url}")
             if e.response.status_code == 429:
                 print("Rate limit exceeded. Waiting 30 seconds before retrying.")
-                sleep(30)
+                sleep(45)
         except httpx.RequestError as e:
             print(f"Attempt {attempt}: Network error on {url} - {e}")
         sleep(delay)
@@ -73,6 +75,11 @@ def get_user_info(user_id):
         'link': f"https://www.roblox.com/users/{user_data.get('id', '')}"
     }
 
+def get_user_friends(user_id):
+    url = f'https://friends.roblox.com/v1/users/{user_id}/friends'
+    friends_data = send_request(url)
+    return friends_data.get('data', [])
+
 def process_group(group_id):
     print(f"Fetching members for group ID: {group_id}")
     group_members = {}
@@ -86,22 +93,36 @@ def process_group(group_id):
         for user in users:
             try:
                 user_info = get_user_info(user['userId'])
-                users_with_info.append(user_info)
+                if user_info:
+                    users_with_info.append(user_info)
+                    friends = get_user_friends(user['userId'])
+                    with open(os.path.join(friends_dir, f"{user['userId']}_friends.json"), "w", encoding="utf-8") as f:
+                        json.dump(friends, f, indent=4)
             except Exception as e:
                 print(f"Failed to fetch info for user {user['userId']}. Error: {e}")
         group_members[role_name] = users_with_info
     
-    # Write to output file with Unicode support for emojis
     with output_lock:
-        with open(f"{group_id}_{output_file}", "a", encoding="utf-8") as file:
+        with open(f"{group_id}_{output_file}", "w", encoding="utf-8") as file:
             json.dump({f"Group_{group_id}": group_members}, file, indent=4, ensure_ascii=False)
             file.write("\n")
 
+if not os.path.exists(friends_dir):
+    os.makedirs(friends_dir)
+
+num_threads = 5
 for group_id in group_ids:
-    if os.path.exists(f"group\\{group_id}_{output_file}"):
-        print(f"Skipping group {group_id} as output file already exists.")
-        continue
-    threads = [Thread(target=process_group, args=(group_id,))]
+    if os.path.exists(os.path.join(folder_name, f"{group_id}_{output_file}")):
+        with open('group_ids.txt', 'r') as file:
+            lines = file.readlines()
+        with open('group_ids.txt', 'w') as file:
+            for line in lines:
+                if line.strip() != group_id:
+                    file.write(line)
+        exit()
+    else:
+        print(f"Processing group {group_id}")
+    threads = [Thread(target=process_group, args=(group_id,)) for _ in range(num_threads)]
     for thread in threads:
         thread.start()
     for thread in threads:
@@ -109,10 +130,9 @@ for group_id in group_ids:
 
 print(f"Data exported successfully")
 
-# Move output file to folder
-
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 for file in os.listdir():
     if file.endswith("_all_members.json"):
         shutil.move(file, os.path.join(folder_name, file))
+
--- search.py
+++ search.py
@@ -2,44 +2,57 @@ import json
 import os
 import unicodedata
 import re
-from config import group_ids
+
+with open('group_ids.txt') as f:
+    group_ids = [line.strip() for line in f]
+
+count = 0
 
 # Function to normalize text for consistent emoji and diacritic handling
 def normalize_text(text):
     normalized_text = unicodedata.normalize('NFKD', text)
     return ''.join(c for c in normalized_text if not unicodedata.combining(c))
 
+links = set()
+
+for group_id in group_ids:
+    if os.path.exists(f'suspicious_users\\suspicious_users_{group_id}.json'):
+        os.remove(f'suspicious_users\\suspicious_users_{group_id}.json')
+
 def append_to_json_file(file_path, document):
     file_path = f'suspicious_users\\{file_path}'
+    
     try:
-        # Read the existing data
+        # Read existing data
         with open(file_path, 'r+', encoding='utf-8') as file:
-            data = json.load(file)
-            
-            # Append the new document
-            data.append(document)
-            
-            # Move the file pointer to the beginning and truncate the file
-            file.seek(0)
-            file.truncate()
-            
-            # Write the updated data back to the file
-            json.dump(data, file, indent=4, ensure_ascii=False)
-            file.write('\n')
-    except FileNotFoundError:
-        # If the file doesn't exist, create it and write the document as the first entry in a list
+            try:
+                data = json.load(file)
+                # Convert list to dictionary if necessary
+                if isinstance(data, list):
+                    data = {entry["id"]: entry for entry in data}
+            except json.JSONDecodeError:
+                data = {}  # Handle empty or corrupted JSON
+
+        user_id = document["id"]
+
+        # Ensure uniqueness by using user ID as key
+        if user_id not in data:
+            data[user_id] = document  # Store only one entry per user
+
+        # Write updated data
         with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump([document], file, indent=4, ensure_ascii=False)
-            file.write('\n')
-    except json.JSONDecodeError:
-        # If the file is empty or not a valid JSON, write the document as the first entry in a list
-        with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump([document], file, indent=4, ensure_ascii=False)
+            json.dump(list(data.values()), file, indent=4, ensure_ascii=False)
             file.write('\n')
 
+    except FileNotFoundError:
+        # If the file doesn't exist, create it and write the first entry
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump([document], file, indent=4, ensure_ascii=False)
+            file.write('\n')
 
 # Function to load JSON data, search bios, and decode Morse code if present
 def search_bio_in_json(file_path, *search_terms):
+    global count
     normalized_terms = [normalize_text(term) for term in search_terms]
 
     try:
@@ -56,7 +69,8 @@ def search_bio_in_json(file_path, *search_terms):
                 bio = normalize_text(user.get("bio", ""))
                 hasVerifiedBadge = user.get("hasVerifiedBadge", "")
                 created = user.get("created", "")
-                isBanned = user.get("isBanned", "")
+                isBanned = user.get("isBanned"),
+                link = user.get("link", "")
 
                 user_doc = {
                     "id": id,
@@ -66,53 +80,47 @@ def search_bio_in_json(file_path, *search_terms):
                     "hasVerifiedBadge": hasVerifiedBadge,
                     "created": created,
                     "isBanned": isBanned,
+                    "link": link,
                     "reason": ""
                 }
+                
+                count += 1
 
-                # Search for terms in plain bio
-                if any(re.search(rf'\b{re.escape(term.lower())}\b', bio.lower()) for term in normalized_terms):
-                    print(f"Username: {username}")
-                    print(f"Display Name: {display_name}")
-                    print(f"Bio: {bio}")
-                    print(f"ID: {id}")
-                    print(f"Created: {created}")
-                    print('reason: bio')
-                    print("-" * 40)
-                    
-                    user_doc["reason"] = "bio"
-                    append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
-                    
-                if any(re.search(rf'\b{re.escape(term.lower())}\b', username.lower()) for term in normalized_terms):
-                    print(f"Username: {username}")
-                    print(f"Display Name: {display_name}")
-                    print(f"Bio: {bio}")
-                    print(f"ID: {id}")
-                    print(f"Created: {created}")
-                    print('reason: username')
-                    print("-" * 40)
-                    
-                    user_doc["reason"] = "username"
-                    append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
-                    
-                if any(re.search(rf'\b{re.escape(term.lower())}\b', display_name.lower()) for term in normalized_terms):
-                    print(f"Username: {username}")
-                    print(f"Display Name: {display_name}")
-                    print(f"Bio: {bio}")
-                    print(f"ID: {id}")
-                    print(f"Created: {created}")
-                    print('reason: display name')
-                    print("-" * 40)
-                    
-                    user_doc["reason"] = "display name"
-                    append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
+                for term in normalized_terms:
+                    if re.search(rf'\b{re.escape(term.lower())}\b', bio.lower()):
+                        user_doc["reason"] = f"bio: {term}"
+                        append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
+                        print(f"{count}. Found {term} in bio of {display_name} link: {link}")
+                        links.add(link)
+
+                    if re.search(rf'\b{re.escape(term.lower())}\b', username.lower()):
+                        user_doc["reason"] = f"username: {term}"
+                        append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
+                        print(f"{count}. Found {term} in username of {display_name} link: {link}")
+                        links.add(link)
+
+                    if re.search(rf'\b{re.escape(term.lower())}\b', display_name.lower()):
+                        user_doc["reason"] = f"display name: {term}"
+                        append_to_json_file(f"suspicious_users_{group_id}.json", user_doc)
+                        print(f"{count}. Found {term} in display name of {display_name} link: {link}")
+                        links.add(link)
 
     except json.JSONDecodeError:
-        print("Error: The file could not be decoded as JSON.")
+        print(f"Error: The file could not be decoded as JSON. {file_path}")
     except FileNotFoundError:
-        print(f"Error: The specified file was not found. {file_path}")
-        
+        print(f"Error: The file was not found. {file_path}")
+
+# Function to search the friends directory
+def search_friends_directory(group_id, *search_terms):
+    friends_file_path = f'friends\\{group_id}_friends.json'
+    search_bio_in_json(friends_file_path, *search_terms)
 
 for group_id in group_ids:
-    file_path = f'D:\\code\\python\\roblox group enumerator\\group\\{group_id}_all_members.json'
-    search_bio_in_json(file_path, 'sub', 'dom', 'fem', 'studio', 'rp', ':3', 'socials', 'trade', 'app', 'blue app', 'h%es', 'btm', 'top', 'switch', 'social', 'step bro', 'step sis', 'tos', 'snow', 'buns', 'bunny', 'lvl','bull', 'pound', 'master', 'rper', 'bals', 'breed', 'yng', 'gorilla', 'cakedup', '💿', '📀', '💽', 'dc', 'maid', 'disk', 'fem boi', 'https://www.roblox.com/users/', 'diaper', 'nappy', 'cons')
+    file_path = f'group\\{group_id}_all_members.json'
+    search_bio_in_json(file_path, 'sub', 'dom', 'fem', 'studio', 'rp', ':3', 'socials', 'trade', 'app', 'blue app', 'blueapp', 'h%es', 'btm', 'top', 'switch', 'social', 'step bro', 'step sis', 'tos', 'snow', 'buns', 'bunny', 'lvl','bull', 'pound', 'master', 'rper', 'bals', 'breed', 'yng', 'gorilla', 'cakedup', '💿', '📀', '💽', 'dc', 'maid', 'disk', 'fem boi', 'https://www.roblox.com/users/', 'diaper', 'nappy', 'cons', 'femb', 'cake', 'hairpull', 'mommy', 'daddy', 'literate', 'thug', 'clap', 'tyrone', 'inch', 'pedo', 'mdni', '🐂', 'remboys', 'latex', 'endowed', 'plow', 'stood', 'morph', 'grape', 'legal', 'blacked', 'own', 'huge', 'stiff', 'stwudioo', 'cd', 'limitless', 'experienced', 'roleplay', '🔁', 'teen', 'erp', 'eboni', 'aced', 'slyt')
+    search_friends_directory(group_id, 'sub', 'dom', 'fem', 'studio', 'rp', ':3', 'socials', 'trade', 'app', 'blue app', 'blueapp', 'h%es', 'btm', 'top', 'switch', 'social', 'step bro', 'step sis', 'tos', 'snow', 'buns', 'bunny', 'lvl','bull', 'pound', 'master', 'rper', 'bals', 'breed', 'yng', 'gorilla', 'cakedup', '💿', '📀', '💽', 'dc', 'maid', 'disk', 'fem boi', 'https://www.roblox.com/users/', 'diaper', 'nappy', 'cons', 'femb', 'cake', 'hairpull', 'mommy', 'daddy', 'literate', 'thug', 'clap', 'tyrone', 'inch', 'pedo', 'mdni', '🐂', 'remboys', 'latex', 'endowed', 'plow', 'stood', 'morph', 'grape', 'legal', 'blacked', 'own', 'huge', 'stiff', 'stwudioo', 'cd', 'limitless', 'experienced', 'roleplay', '🔁', 'teen', 'erp', 'eboni', 'aced', 'slyt')
+
+with open('all_links.txt', 'w', encoding='utf-8') as f:
+    for link in links:
+        f.write(f"{link}\n")
 
